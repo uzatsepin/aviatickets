078f807ce598a54cba3633eb8261bc1044d3edfc
Andrii Zatsepin <uzatsepin@gmail.com>
2022-05-02 13:25:00 +0300

    fix: added mobile responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   purge: ['./src/**/*.html', '.src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    screens: {
+      mobile: '360px',
+      tablet: '600px',
+      desktop: '1200px',
+    },
     extend: {},
   },
   plugins: [],
